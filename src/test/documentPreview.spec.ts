e4515c4565e8b75ad815d561272934db23e63be9
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-12-23 10:44:53 +0800

    lint fixed

--- src/test/documentPreview.spec.ts
+++ src/test/documentPreview.spec.ts
@@ -14,23 +14,28 @@ const validateTextContent = async (t: TestController, component: Selector, texts
   texts.reduce(async (_prev, curr) => t.expect(component.textContent).contains(curr), Promise.resolve());
 
 /* eslint-disable jest/expect-expect, jest/require-top-level-describe */
-test("form preview is rendered correctly", async (t: TestController) => {
-    await t.setFilesToUpload("input[type=file]", [Config]);
-    await FormHeader.with({ visibilityCheck: true })();
-    await t.setFilesToUpload("input[type=file]", [Data]);
+test("form preview is rendered correctly", async () => {
+  await new Promise(t => {
+    await new Promise(async done => {
+      await t.setFilesToUpload("input[type=file]", [Config]);
+      await FormHeader.with({ visibilityCheck: true })();
+      await t.setFilesToUpload("input[type=file]", [Data]);
 
-    await validateTextContent(t, FormHeader, ["Show Preview"]);
-    await t.click(ShowPreview).setPageLoadTimeout(10000);
-    await validateTextContent(t, PreviewModal, ["Document Preview"]);
-    await t.switchToIframe(IframeBlock);
-    await SampleTemplate.with({ visibilityCheck: true })();
+      await validateTextContent(t, FormHeader, ["Show Preview"]);
+      await t.click(ShowPreview).setPageLoadTimeout(10000);
+      await validateTextContent(t, PreviewModal, ["Document Preview"]);
+      await t.switchToIframe(IframeBlock);
+      await SampleTemplate.with({ visibilityCheck: true })();
 
-    await validateTextContent(t, SampleTemplate, [
-      "Demo Shipper",
-      "BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT",
-      "Demo Consignee",
-      "China Port",
-      "Green Apples",
-      "THREE/3"
-    ]);
+      await validateTextContent(t, SampleTemplate, [
+        "Demo Shipper",
+        "BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT",
+        "Demo Consignee",
+        "China Port",
+        "Green Apples",
+        "THREE/3"
+      ]);
+      done();
+    });
+  });
 });
--- src/test/jsonSchemaRendering.spec.ts
+++ src/test/jsonSchemaRendering.spec.ts
@@ -11,21 +11,26 @@ const validateTextContent = async (t: TestController, component: Selector, texts
   texts.reduce(async (_prev, curr) => t.expect(component.textContent).contains(curr), Promise.resolve());
 
 /* eslint-disable jest/expect-expect, jest/require-top-level-describe */
-test("config schema is rendered correctly", async (t: TestController) => {
-    const container = Selector("#document-dropzone");
-    await container();
-    await validateTextContent(t, DropzoneContainer, ["Drag 'n' drop TradeTrust configuration file here"]);
+test("config schema is rendered correctly", async () => {
+  await new Promise(t => {
+    await new Promise(async done => {
+      const container = Selector("#document-dropzone");
+      await container();
+      await validateTextContent(t, DropzoneContainer, ["Drag 'n' drop TradeTrust configuration file here"]);
 
-    await t.setFilesToUpload("input[type=file]", [Config]);
+      await t.setFilesToUpload("input[type=file]", [Config]);
 
-    await FormHeader.with({ visibilityCheck: true })();
+      await FormHeader.with({ visibilityCheck: true })();
 
-    await validateTextContent(t, FormBody, [
-      "DEMO CNM",
-      "Template Renderer",
-      "Issuers of the document",
-      "Port of Loading",
-      "Recipient of the container",
-      "Consignee"
-    ]);
+      await validateTextContent(t, FormBody, [
+        "DEMO CNM",
+        "Template Renderer",
+        "Issuers of the document",
+        "Port of Loading",
+        "Recipient of the container",
+        "Consignee"
+      ]);
+      done();
+    });
+  });
 });
