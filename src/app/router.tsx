9baa4c5e9b77c7840e95a519acc78d24fa03b1f0
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-12-24 11:11:41 +0800

    validated route added

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,16 +1,30 @@
-import React, { ReactElement, Suspense, lazy } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { ReactElement, Suspense, lazy, useContext, ComponentType } from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { isEmpty } from "lodash";
+
+import { ConfigContext } from "../contexts/ConfigurationContext";
 
 const ConfigDropzoneContainer = lazy((): any => import("../components/ConfigDropzone"));
 const FormDisplay = lazy((): any => import("../components/FormDisplay"));
 const Web3EnabledWidget = lazy((): any => import("../components/ExampleWeb3Widget"));
+
+interface RouteProps {
+  component: ComponentType<any>;
+  exact: boolean;
+  path: string;
+}
+
+const ValidatedRoute = ({component: Component, ...rest}: RouteProps) => {
+  const { config } = useContext(ConfigContext);
+  return (<Route {...rest} render={props => !isEmpty(config) ? <Component {...props} /> : <Redirect to='/' />} />);
+}
 
 export const Routes = (): ReactElement => (
   <Router>
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
         <Route exact path="/" component={ConfigDropzoneContainer} />
-        <Route exact path="/form" component={FormDisplay} />
+        <ValidatedRoute exact path="/form" component={FormDisplay} />
         <Route exact path="/web3-example" component={Web3EnabledWidget} />
       </Switch>
     </Suspense>
