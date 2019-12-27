77ec53d8dd6b58372741b47ac45cc2322da40dda
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-12-27 16:04:04 +0800

    added cnfirmation modal for issue document.

--- src/components/Common/PopupModal.tsx
+++ src/components/Common/PopupModal.tsx
@@ -7,9 +7,16 @@ interface PopupModalProps {
     [key: string]: string | number;
   };
   toggleDisplay: (val: boolean) => void;
+  footerComponent?: ReactElement;
 }
 
-export const PopupModal = ({ children, toggleDisplay, title, containerStyle }: PopupModalProps): ReactElement => (
+export const PopupModal = ({
+  children,
+  toggleDisplay,
+  title,
+  containerStyle,
+  footerComponent
+}: PopupModalProps): ReactElement => (
   <div id="preview-modal" className="modal" role="dialog" style={{ display: "block", overflow: "scroll" }}>
     <div className="modal-dialog" style={containerStyle}>
       <div className="modal-content">
@@ -21,9 +28,13 @@ export const PopupModal = ({ children, toggleDisplay, title, containerStyle }: P
         </div>
         <div className="modal-body">{children}</div>
         <div className="modal-footer">
-          <button type="button" className="btn btn-default" onClick={() => toggleDisplay(false)}>
-            Close
-          </button>
+          {footerComponent ? (
+            footerComponent
+          ) : (
+            <button type="button" className="btn btn-default" onClick={() => toggleDisplay(false)}>
+              Close
+            </button>
+          )}
         </div>
       </div>
     </div>
--- src/components/FormDisplay/index.tsx
+++ src/components/FormDisplay/index.tsx
@@ -6,25 +6,56 @@ import { FormDataContext } from "../../contexts/FormDataContext";
 import { ConfigContext } from "../../contexts/ConfigurationContext";
 import { UploadDataView } from "./UploadDataView";
 import { DisplayPreview } from "./DisplayPreview";
+import { PopupModal } from "../Common/PopupModal";
 
 const HeaderDiv = styled.div`
   background-color: dimgray;
   text-align: right;
 `;
 
+interface FooterModalProps {
+  toggleConfirmationModal: (i: boolean) => void;
+  publishDocument: (val: boolean) => void;
+}
+
+//add publish document method to publish it on blockchain.
+const FooterModal = ({ toggleConfirmationModal, publishDocument }: FooterModalProps): ReactElement => (
+  <>
+    <button type="button" className="btn btn-default" onClick={() => toggleConfirmationModal(false)}>
+      Close
+    </button>
+    <button type="button" className="btn btn-primary" onClick={() => publishDocument(false)}>
+      Ok
+    </button>
+  </>
+);
+
 const FormDisplay = (): ReactElement => {
   const { documentsMeta, setDocumentMeta, setWrappedDocument } = useContext(FormDataContext);
   const [activeTab] = useState(0);
+  const [showConfirmationModal, toggleConfirmationModal] = useState(false);
   const { config } = useContext(ConfigContext);
 
   const handleSubmit = (formValues: Document): void => {
     documentsMeta.splice(activeTab, 1, formValues);
     setDocumentMeta(documentsMeta);
     setWrappedDocument(formValues);
+    toggleConfirmationModal(true);
   };
 
   return (
     <>
+      {showConfirmationModal && (
+        <PopupModal
+          title="Publish Document"
+          toggleDisplay={toggleConfirmationModal}
+          footerComponent={
+            <FooterModal toggleConfirmationModal={toggleConfirmationModal} publishDocument={toggleConfirmationModal} />
+          }
+        >
+          <div>Are you sure you want to publish document. </div>
+        </PopupModal>
+      )}
       <HeaderDiv id="form-header" className="container">
         {documentsMeta[activeTab] && <DisplayPreview document={documentsMeta[activeTab]} />}
         <UploadDataView />
