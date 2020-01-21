de16f8ffb02ca36c6ee81b803b9b6546725491b7
Sim Boon Long <simboonlong@gmail.com>
2020-01-21 11:36:20 +0800

    fix: overflow context in preview

--- src/components/Common/PopupModal.tsx
+++ src/components/Common/PopupModal.tsx
@@ -27,29 +27,31 @@ export const PopupModal = ({
 
   return (
     <>
-      <div id="preview-modal" className={`modal fade ${isFadeIn && "show"}`} role="dialog" style={{ display: "block" }}>
-        <div className="modal-dialog modal-dialog-centered" style={containerStyle}>
-          <form className="modal-content" onSubmit={() => toggleDisplay(false)}>
-            <div className="modal-header">
-              <h4 className="modal-title">{title}</h4>
-              <button type="button" className="close" onClick={() => toggleDisplay(false)}>
-                &times;
-              </button>
-            </div>
-            <div className="modal-body">{children}</div>
-            <div className="modal-footer">
-              {footerComponent ? (
-                footerComponent
-              ) : (
-                <button type="button" className="btn btn-default" onClick={() => toggleDisplay(false)}>
-                  Close
+      <div className="modal-open">
+        <div id="preview-modal" className={`modal fade ${isFadeIn && "show"}`} role="dialog" style={{ display: "block" }}>
+          <div className="modal-dialog modal-dialog-centered" style={containerStyle}>
+            <form className="modal-content" onSubmit={() => toggleDisplay(false)}>
+              <div className="modal-header">
+                <h4 className="modal-title">{title}</h4>
+                <button type="button" className="close" onClick={() => toggleDisplay(false)}>
+                  &times;
                 </button>
-              )}
-            </div>
-          </form>
+              </div>
+              <div className="modal-body">{children}</div>
+              <div className="modal-footer">
+                {footerComponent ? (
+                  footerComponent
+                ) : (
+                  <button type="button" className="btn btn-default" onClick={() => toggleDisplay(false)}>
+                    Close
+                  </button>
+                )}
+              </div>
+            </form>
+          </div>
         </div>
+        <div className={`modal-backdrop fade ${isFadeIn && "show"}`} />
       </div>
-      <div className={`modal-backdrop fade ${isFadeIn && "show"}`} />
     </>
   );
 };
